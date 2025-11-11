750058ee1db39ffa1ad44b175ac085c4c405c7ac
Ricky <win123id@gmail.com>
2025-11-11 12:07:50 +0700

    remove header from auth

--- components/layout/main-layout.tsx
+++ components/layout/main-layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import { Header } from "./header";
 import { Footer } from "./footer";
 
@@ -6,15 +9,20 @@ interface MainLayoutProps {
 }
 
 export function MainLayout({ children }: MainLayoutProps) {
+  const pathname = usePathname();
+  
+  // Don't show header and footer for auth routes
+  const isAuthRoute = pathname?.startsWith("/auth");
+  
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
+      {!isAuthRoute && <Header />}
       <main className="flex-1 bg-muted/30">
         <div className="container py-8 px-6">
           {children}
         </div>
       </main>
-      <Footer />
+      {!isAuthRoute && <Footer />}
     </div>
   );
 }
