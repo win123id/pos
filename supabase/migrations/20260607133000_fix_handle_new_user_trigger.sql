e6198468ed3888ca852b3a82c549df117867ca52
Ricky Ethan <win123id@gmail.com>
2026-06-07 21:13:51 +0700

    Fix new user profile trigger

---
+++ supabase/migrations/20260607133000_fix_handle_new_user_trigger.sql
@@ -0,0 +1,17 @@
+create or replace function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer
+set search_path = public
+as $$
+begin
+  insert into public.profiles (id, full_name, role)
+  values (
+    new.id,
+    null,
+    'user'
+  );
+
+  return new;
+end;
+$$;
